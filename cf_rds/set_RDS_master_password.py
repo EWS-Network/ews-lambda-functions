6b3ea5ea0689d3a2ee339ad29d4978223f93538d
John M <john@ews-network.net>
2017-06-16 14:20:03 +0000

    Test with CF for creation and deletion and update. Deletion works fine

--- cf_rds/set_RDS_master_password.py
+++ cf_rds/set_RDS_master_password.py
@@ -16,12 +16,73 @@ import urlparse
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
 
+#
+# GLOBAL VARIABLES
+#
+
+
+table_keyid = 'rds_id'
+
+
+def encrypt_password(key_id=None, password=None):
+    """
+    Function to encrypt the password with KMS
+    :param: key_id: KMS Key Id
+    :param: password to encrypt
+    :return: String password in base64
+    """
+    client = boto3.client('kms')
+    try:
+        encrypted = client.encrypt(
+            KeyId=key_id,
+            Plaintext=password
+        )
+        encrypted_b64 = base64.b64encode(encrypted['CiphertextBlob'])
+        return {'encrypted': True, 'password': encrypted_b64}
+    except Exception as e:
+        return {'encrypted': False, 'Reason': e}
+
+
+def generate_random_string(string_length=20, underscore=False):
+    """
+    Generates a random string sent back to CF
+    :param string_length: Length of the string in number of characters
+    :return: String of the string
+    """
+    generated_string = ''
+    char_set = string.ascii_uppercase + string.ascii_lowercase + string.digits + '-'
+    while '-' not in generated_string or \
+          generated_string.endswith('-') or \
+          generated_string.startswith('-'):
+        generated_string = ''.join(random.sample(char_set * 6, int(string_length)))
+    if underscore:
+        return generated_string.replace('-', '_')
+    return generated_string
+
+
+def add_master_creds(table_name,
+                     stack_name,
+                     key_id):
+    """
+    Create Master function
+    """
+    username = generate_random_string(10, True)
+    password = generate_random_string(21)
+    encrypted_password = encrypt_password(key_id, password)
+    if encrypted_password['encrypted']:
+        return put_password_in_dynamodb(
+            table_name,
+            stack_name,
+            username,
+            encrypted_password['password']
+        )
+    return encrypted_password
+
 
 def put_password_in_dynamodb(table_name,
                              stack_name,
                              user_name,
                              b64_encrypted_password,
-                             attribute_name,
                              update=False):
     """
     Function to store the b64 password in DynamoDB
@@ -39,7 +100,7 @@ def put_password_in_dynamodb(table_name,
 
     try:
         response = table.query(
-            KeyConditionExpression=Key(attribute_name).eq(hash_string)
+            KeyConditionExpression=Key(table_keyid).eq(hash_string)
         )
     except Exception as e:
         return {'added': False, 'Reason': e}
@@ -49,11 +110,12 @@ def put_password_in_dynamodb(table_name,
 
     elif (response['Count'] != 0 and update) or \
          (response['Count'] == 0):
-
+        print response['Count']
+        print update
         try:
             response = table.put_item(
                 Item={
-                    attribute_name : hash_string,
+                    table_keyid : hash_string,
                     'user_name' : user_name,
                     'b64_password': b64_encrypted_password
                 }
@@ -63,24 +125,41 @@ def put_password_in_dynamodb(table_name,
             return {'added': False, 'Reason': e}
 
 
-def add_master_creds(table_name,
-                     stack_name,
-                     key_id):
+def delete_from_dyndb(table_name,
+                      stack_name):
     """
-    Master function
+    Function to store the b64 password in DynamoDB
+    :param table_name: Name of the table
+    :param stack_name: Name of the RDS Stack
+    :param update: Boolean() to determine whether or not the table should be updated
     """
-    username = generate_random_string(10, True)
-    password = generate_random_string(21)
-    encrypted_password = encrypt_password(key_id, password)
-    if encrypted_password['encrypted']:
-        return put_password_in_dynamodb(
-            table_name,
-            stack_name,
-            username,
-            encrypted_password['password'],
-            'rds_id'
+
+    hash_string = hashlib.sha256(stack_name).hexdigest().strip()
+    client = boto3.resource('dynamodb')
+    table = client.Table(table_name)
+
+    try:
+        response = table.query(
+            KeyConditionExpression=Key(table_keyid).eq(hash_string)
         )
-    return encrypted_password
+    except Exception as e:
+        return {'deleted': False, 'Reason': e}
+
+    if response['Count'] != 1:
+        return {'deleted': True, 'Reason': "Entry doesnt exist - no delete required"}
+
+    elif response['Count'] == 1:
+        try:
+            response = table.delete_item(
+                Key={
+                    table_keyid : hash_string
+                }
+            )
+            return {'deleted': True, 'Reason': 'Successfully Deleted from DynDB'}
+        except Exception as e:
+            return {'deleted': False, 'Reason': e}
+
+# LAMBDA SPECIFIC
 
 
 def lambda_handler(event, context):
@@ -105,28 +184,37 @@ def lambda_handler(event, context):
 
     # There is nothing to do for a delete or update request
 
-    if (event['RequestType'] == 'Delete') or \
-       (event['RequestType'] == 'Update'):
+    if not check_event_keys(event, ['TableName', 'StackName']):
+        return send_response(
+            event,
+            response,
+            status='FAILED',
+            reason='Function parameters missing'
+        )
+
+    if event['RequestType'] == 'Update':
         return send_response(event, response)
 
-
-    for key in ['KeyId', 'TableName']:
-        if not key in event['ResourceProperties'].keys():
-            return send_response(
-                event,
-                response,
-                status='FAILED',
-                reason='The properties KeyId and TableName must not be empty'
-            )
-
-    creds = add_master_creds(
-        event['ResourceProperties']['TableName'],
-        event['ResourceProperties']['StackName'],
-        event['ResourceProperties']['KeyId']
+    if event['RequestType'] == 'Create':
+        creds = add_master_creds(
+            event['ResourceProperties']['TableName'],
+            event['ResourceProperties']['StackName'],
+            event['ResourceProperties']['KeyId']
         )
-    if not creds['added']:
-        response['Status'] = 'FAILED'
-    response['Reason'] = creds['Reason']
+        if not creds['added']:
+            response['Status'] = 'FAILED'
+        response['Reason'] = creds['Reason']
+
+    elif event['RequestType'] == 'Delete':
+        deleted = delete_from_dyndb(
+            event['ResourceProperties']['TableName'],
+            event['ResourceProperties']['StackName']
+        )
+        if not deleted['deleted']:
+            response['Status'] = 'FAILED'
+            response['Reason'] = deleted['Reason']
+        return send_response(event, response)
+
     return send_response(
         event,
         response
@@ -134,6 +222,7 @@ def lambda_handler(event, context):
 
 
 # NEVER CHANGE THE SEND RESPONSE FUNCTION
+
 
 def send_response(request, response, status=None, reason=None):
     """
@@ -160,43 +249,18 @@ def send_response(request, response, status=None, reason=None):
     return response
 
 
-def generate_random_string(string_length=20, underscore=False):
+def check_event_keys(event, keys_list):
     """
-    Generates a random string sent back to CF
-    :param string_length: Length of the string in number of characters
-    :return: String of the string
+    Function to check all the event keys are present for the function to work
     """
-    generated_string = ''
-    char_set = string.ascii_uppercase + string.ascii_lowercase + string.digits + '-'
-    while '-' not in generated_string or \
-          generated_string.endswith('-') or \
-          generated_string.startswith('-'):
-        generated_string = ''.join(random.sample(char_set * 6, int(string_length)))
-    if underscore:
-        return generated_string.replace('-', '_')
-    return generated_string
+    for key in keys_list:
+        if not key in event['ResourceProperties'].keys():
+            return False
+    return True
 
 
-def encrypt_password(key_id=None, password=None):
-    """
-    Function to encrypt the password with KMS
-    :param: key_id: KMS Key Id
-    :param: password to encrypt
-    :return: String password in base64
-    """
-    client = boto3.client('kms')
-    try:
-        encrypted = client.encrypt(
-            KeyId=key_id,
-            Plaintext=password
-        )
-        encrypted_b64 = base64.b64encode(encrypted['CiphertextBlob'])
-        return {'encrypted': True, 'password': encrypted_b64}
-    except Exception as e:
-        return {'encrypted': False, 'Reason': e}
 
-
-# FOR CLI TESTING
+# FOR CLI USAGE
 
 
 if __name__ == '__main__':
@@ -206,7 +270,12 @@ if __name__ == '__main__':
     parser.add_argument('--stack-name', '-s', help="Name of the stack", required=True)
     parser.add_argument('--key-id', '-k', help="KMS Key Id", required=True)
     parser.add_argument('--table-name', '-t', help="Name of the DynamoDB Table", required=True)
+    parser.add_argument('--delete', help="Test the delete function", required=False, action='store_true')
 
     args = parser.parse_args()
-    creds = add_master_creds(args.table_name, args.stack_name, args.key_id)
-    print(creds['Reason'])
+    if not args.delete:
+        creds = add_master_creds(args.table_name, args.stack_name, args.key_id)
+        print(creds['Reason'])
+    else:
+        deletion = delete_from_dyndb(args.table_name, args.stack_name)
+        print deletion['Reason']
