3581d6f2d52a9726cde45c2d416d118d4b01ec6b
John M <john@ews-network.net>
2017-06-20 08:10:08 +0000

    Added the Lambda part to work with CF

--- network/test_network.py
+++ network/test_network.py
@@ -5,6 +5,15 @@ POC for subnet calculator
 """
 
 import ipaddress
+import base64
+import json
+import random
+import string
+import hashlib
+import uuid
+import httplib
+import urlparse
+import boto3
 
 
 def how_many_fit(max, quantity, size):
@@ -35,7 +44,7 @@ def find_prefix(to_fit, cidr):
         else:
             prefix += 1
 
-def define_supersubnet(azs, layers, network, prefix, block_ips):
+def define_supersubnet(azs,  network, prefix, block_ips, largest_powered_two, cidr):
     """
     """
     largest = largest_powered_two
@@ -59,14 +68,13 @@ def find_power_two(target):
             return 2 ** (i - 1)
 
 
-if __name__ == '__main__':
-
-    azs = 3
-    layers = 3
-
-    cidr = u'10.201.0.0/20'
+def get_subnets(cidr, azs):
+    """
+    Main function
+    """
+    cidr = unicode(cidr)
     vpc_net = ipaddress.IPv4Network(cidr)
-    number_ips = vpc_net.num_addresses - 2
+    number_ips = int(vpc_net.num_addresses - 2)
 
     print "MAX NUM HOSTS", number_ips
 
@@ -97,18 +105,18 @@ if __name__ == '__main__':
     largest_powered_two = find_power_two(number_ips / azs)
     supersized_prefix = find_prefix(largest_powered_two, cidr)
 
-    public_block = define_supersubnet(azs, layers, vpc_net.network_address, vpc_net._prefixlen, public_ips_num)
+    public_block = define_supersubnet(azs,  vpc_net.network_address, vpc_net._prefixlen, public_ips_num, largest_powered_two, cidr)
     exclude_public = list(vpc_net.address_exclude(public_block))
     if len(exclude_public) == 2:
         app_block = define_supersubnet(
-            azs, layers,
+            azs,
             exclude_public[0].network_address,
-            vpc_net._prefixlen, app_ips_num
+            vpc_net._prefixlen, app_ips_num, largest_powered_two, cidr
         )
         db_block = define_supersubnet(
-            azs, layers,
+            azs,
             exclude_public[1].network_address,
-            vpc_net.prefixlen, db_ips_num
+            vpc_net.prefixlen, db_ips_num, largest_powered_two, cidr
         )
 
     print public_block, app_block, db_block
@@ -137,7 +145,99 @@ if __name__ == '__main__':
     for subnet in list(db_subnets):
         db_subnets_cidr.append(subnet.with_prefixlen)
 
-    print db_subnets_cidr
-    print app_subnets_cidr
-    print public_subnets_cidr
+    # print db_subnets_cidr
+    # print app_subnets_cidr
+    # print public_subnets_cidr
+    return (public_subnets_cidr, app_subnets_cidr, db_subnets_cidr)
+
+
+# NEVER CHANGE THE SEND RESPONSE FUNCTION
+
+def send_response(request, response, status=None, reason=None):
+    """
+    Send our response to the pre-signed URL supplied by CloudFormation
+    If no ResponseURL is found in the request, there is no place to send a
+    response. This may be the case if the supplied event was for testing.
+    :return: response object
+    """
+
+    if status is not None:
+        response['Status'] = status
+
+    if reason is not None:
+        response['Reason'] = reason
+
+    if 'ResponseURL' in request and request['ResponseURL']:
+        try:
+            url = urlparse.urlparse(request['ResponseURL'])
+            body = json.dumps(response)
+            https = httplib.HTTPSConnection(url.hostname)
+            https.request('PUT', url.path + '?' + url.query, body)
+        except:
+            print("Failed to send the response to the provdided URL")
+    return response
+
+
+def lambda_handler(event, context):
+    """
+    AWS Lambda function handler
+    :param event: Lambda event data
+    :param context: Lambda defined context params
+    :return: Calls for send_response when the code could be executed without problem
+    """
+
+    response = {
+    'StackId': event['StackId'],
+    'RequestId': event['RequestId'],
+    'LogicalResourceId': event['LogicalResourceId'],
+    'Status': 'SUCCESS'
+    }
+
+    if 'PhysicalResourceId' in event:
+        response['PhysicalResourceId'] = event['PhysicalResourceId']
+    else:
+        response['PhysicalResourceId'] = str(uuid.uuid4())
+
+    # There is nothing to do for a delete or update request
+
+    if (event['RequestType'] == 'Delete') or \
+       (event['RequestType'] == 'Update'):
+        return send_response(event, response)
+
+
+    for key in ['CIDR']:
+        if not key in event['ResourceProperties'].keys():
+            return send_response(
+                event,
+                response,
+                status='FAILED',
+                reason='The properties TableName and StackName must be present'
+            )
+
+    client = boto3.client('ec2')
+    azs = len(client.describe_availability_zones()['AvailabilityZones'])
+    subnets = get_subnets(event['ResourceProperties']['CIDR'], azs)
+    response['Data'] = {}
+    response['Data']['PublicSubnets'] = subnets[0]
+    response['Data']['AppSubnets'] = subnets[1]
+    response['Data']['DBSubnets'] = subnets[2]
+    response['Reason'] = 'FETCHING SUBNETS'
+    return send_response(
+        event,
+        response
+    )
+
+
+if __name__ == '__main__':
+
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Splits a CIDR to generate VPC subnets")
+    parser.add_argument("--cidr", help="CIDR", required=True)
+    parser.add_argument("--azs", help="AZs", required=True)
+
+    args = parser.parse_args()
+
+    subnets = get_subnets(args.cidr, int(args.azs))
+    print subnets
 
